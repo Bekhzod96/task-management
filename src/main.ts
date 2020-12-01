98298fd0a2512ebbb6a0a3c71366a4e1891fd07d
Bekhzod Akhrorov <bekhzod.akhrorov@Admins-MacBook-Pro-4.local>
2020-12-01 18:18:54 +0200

    Configuration added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as config from 'config';
 async function bootstrap() {
+  const { port } = process.env.PORT || config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  app.enableCors();
   await app.listen(port);
   logger.log(`Applicaiton listening on port ${port}`);
 }
